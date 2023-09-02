709f2f33cdcc77853f1cd36e9ca72f2f43c51eb1
sanjos7777 <aleksandr.schirskiy@gmail.com>
2023-09-02 22:01:33 +0300

    module1

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
 print ("hello world")
 print("Hello Git")
-print("Hello")
+#print("Hello") # at the beggining - meaning that this string commented and doesn't work like a code
+
+# nmbers
+y = 8**3+4*(2+2)
+x = 3.3 + 2j
+print (x, y)
+
+# f- string
+a = "hello "
+b = "world"
+c= a+b
+print(c)
+name = "Jonh"
+hello_string = f"Hello, {name}"
+print (hello_string)
+
+# boolean
+e = True
+d = False
+age = 18
+adult1 = age >= 18 
+print(adult1) # true
+print ("should be false", a == b) # false
+
+# input in Console
+
+# e = input ("введдіть своє імʼя: ")
+# age1 = input ("введдіть свій вік: ")
+
+#input_output = f"Hello, how is it going, {e}. You are {age1} years old and you are allowed to visite this site"
+# print(input_output)
+pi = float(age)
+print (pi)
+print (type (pi))
